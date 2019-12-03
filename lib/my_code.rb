55cc828076c6dd56fda96e37afc9a3c1a0a66974
James Rogers <jarogers095@gmail.com>
2019-12-03 05:52:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,37 @@
 # My Code here....
-def map_to_negativize(collection)
-  negative_collection = []
+def map_to_negativize(source_array)
+  negative_array = []
   
-  collection.each do |element|
-    negative_collection.push(element * -1)
+  source_array.each do |element|
+    negative_array.push(element * -1)
   end
   
-  return negative_collection
+  return negative_array
+end
+
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+
+def map_to_double(source_array)
+  doubled_array = []
+  
+  source_array.each do |element|
+    doubled_array.push(element * 2)
+  end
+  
+  return doubled_array
+end
+
+
+def map_to_square(source_array)
+  squared_array = []
+  
+  source_array.each do |element|
+    squared_array.push(element * element)
+  end
+  
+  return squared_array
 end
